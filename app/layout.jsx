a52b855e0d298cc7a41425475ab30950a6fe315b
Piaowei <msptonmoy@gmail.com>
2023-11-29 18:40:19 +0600

    refactored(mix):project updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,20 +11,28 @@ const poppins = Poppins({
 export const metadata = {
   title: 'Mohammad Shariar Parvez',
   description: 'Full Stack Developer Portfolio Website',
+  image: '/public/assets/head.PNG',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className='scroll-smooth'>
-      <body className={poppins.className}>
-        <main className='flex flex-row'>
-          <SideMenu />
-          <section className='overflow-hidden pl-[300px] max-lg:pl-[146px] w-full min-h-screen max-md:pl-0'>
-            <TopMenu />
-            {children}
-          </section>
-        </main>
-      </body>
-    </html>
+    <>
+      {/* Add the link to your favicon here */}
+
+      {/* Add the meta image tag */}
+      <meta property='og:image' content={metadata.image} />
+
+      <html lang='en' className='scroll-smooth'>
+        <body className={poppins.className}>
+          <main className='flex flex-row'>
+            <SideMenu />
+            <section className='overflow-hidden pl-[300px] max-lg:pl-[146px] w-full min-h-screen max-md:pl-0'>
+              <TopMenu />
+              {children}
+            </section>
+          </main>
+        </body>
+      </html>
+    </>
   );
 }
